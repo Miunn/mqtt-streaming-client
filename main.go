3b7e7f32e2cfa73fa5dc1aee4d9be8da1e85c096
Miunn <remcaulier@orange.fr>
2024-09-27 14:31:23 +0200

    MQTT connect POC

--- main.go
+++ main.go
@@ -1,8 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	message := "Hello, World!"
-	fmt.Println(message)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	BROKER_URL := os.Getenv("BROKER_URL")
+	BROKER_PORT := os.Getenv("BROKER_PORT")
+	BROKER_USER := os.Getenv("BROKER_USER")
+	BROKER_PASSWORD := os.Getenv("BROKER_PASSWORD")
+	BROKER_STREAMING_CLIENT_ID := os.Getenv("BROKER_STREAMING_CLIENT_ID")
+	BROKER_STREAMING_TOPIC := os.Getenv("BROKER_STREAMING_TOPIC")
+
+	var clientOptions = mqtt.NewClientOptions()
+	clientOptions.AddBroker(fmt.Sprintf("%s:%s", BROKER_URL, BROKER_PORT))
+	clientOptions.SetClientID(BROKER_STREAMING_CLIENT_ID)
+	clientOptions.SetUsername(BROKER_USER)
+	clientOptions.SetPassword(BROKER_PASSWORD)
+	var client = mqtt.NewClient(clientOptions)
+
+	token := client.Connect()
+	token.Wait()
+	fmt.Println("Connect: ", token.Error())
+
+	token = client.Publish(BROKER_STREAMING_TOPIC, 0, false, "Hello, MQTT!")
+	token.Wait()
+	fmt.Println("Published: ", token.Error())
 }
