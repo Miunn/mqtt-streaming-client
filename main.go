7f493af6193d9ffcc64ebfd15c84e188cfc3746b
maximezim <maxime.zimmermann57@gmail.com>
2024-10-21 02:14:55 +0200

    changed to mp4

--- main.go
+++ main.go
@@ -15,22 +15,30 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	// Retrieve environment variables
 	BROKER_URL := os.Getenv("BROKER_URL")
 	BROKER_PORT := os.Getenv("BROKER_PORT")
-	BROKER_USER := os.Getenv("BROKER_USER")
+	BROKER_USER := os.Getenv("BROKER_USERNAME")
 	BROKER_PASSWORD := os.Getenv("BROKER_PASSWORD")
 	BROKER_STREAMING_CLIENT_ID := os.Getenv("BROKER_STREAMING_CLIENT_ID")
 
+	// Set up MQTT client options
 	var clientOptions = mqtt.NewClientOptions()
-	clientOptions.AddBroker(fmt.Sprintf("%s:%s", BROKER_URL, BROKER_PORT))
+	clientOptions.AddBroker(fmt.Sprintf("tcp://%s:%s", BROKER_URL, BROKER_PORT))
 	clientOptions.SetClientID(BROKER_STREAMING_CLIENT_ID)
 	clientOptions.SetUsername(BROKER_USER)
 	clientOptions.SetPassword(BROKER_PASSWORD)
-	var client = mqtt.NewClient(clientOptions)
 
+	client := mqtt.NewClient(clientOptions)
 	token := client.Connect()
 	token.Wait()
-	fmt.Println("Connect: ", token.Error())
+	if token.Error() != nil {
+		log.Fatalf("Failed to connect to MQTT broker: %v", token.Error())
+	}
+	log.Println("Connected to MQTT broker")
 
-	RunExampleStream("videos/test.avi", client)
+	RunExampleStream("videos/test.mp4", client)
+
+	client.Disconnect(250)
+	log.Println("Disconnected from MQTT broker")
 }
